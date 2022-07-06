3c596553f64152d1234c7842a96869f3fa09be07
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-05 19:08:22 -0700

    pls

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
-/** 
- *  main - print putchar
- *  Return: always 0
+/**
+ * main - prints "_putchar"
+ *
+ * Return: 0
  */
 
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+	char ch[8] = "_putchar";
+	int i;
 
-return (0);
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+
+	return (0);
 }
