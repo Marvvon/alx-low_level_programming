c162b3613903df54bf087f9e37b76f08045ad6d6
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-05 20:12:54 -0700

    7

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
- *  print_alphabet - entry point
- *  Return: 0 success
+ * print_alphabet - entry point
+ * Return: 0 Success
  */
 void print_alphabet(void)
 {
 	char j;
-	
+
 	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar(j);
