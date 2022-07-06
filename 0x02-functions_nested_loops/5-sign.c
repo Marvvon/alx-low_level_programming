836a59a16452bcfe1e280574f4115198f7117de2
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-06 09:21:08 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign -entry point
+ * @n: numbers to check
+ * Return: 0 or 1
+*/
+int print_sign(int n)
+{
+	int numz;
+        
+        if (n > 0)
+	{
+	 value = 1;
+         _putchar('+');
+        }
+       else if (n == 0)
+       {
+        value = 0;
+        _putchar('0');
+       }
+       else
+       {
+	value = -1;
+        _putchar('-')
+       }
+       
+       return (numz);
+}       
