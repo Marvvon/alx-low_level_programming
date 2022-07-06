dd2fb73eb1e0915a77f14ee85ee5e8c8f275a5cb
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-06 09:33:15 -0700

    new,

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,26 @@
 #include "main.h"
 /**
- * print_sign -entry point
- * @n: numbers to check
- * Return: 0 or 1
+ * print_sign - entry point
+ * @n : number to check
+ * Return:0 or 1
 */
 int print_sign(int n)
 {
 	int numz;
-        
-        if (n > 0)
+
+	if (n > 0)
 	{
-	 value = 1;
-         _putchar('+');
-        }
-       else if (n == 0)
-       {
-        value = 0;
-        _putchar('0');
-       }
-       else
-       {
-	value = -1;
-        _putchar('-')
-       }
-       
-       return (numz);
-}       
+		numz = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		numz = 0;
+		_putchar('0');
+	}
+	else
+	{
+		numz = -1;
+		_putchar('-');
+	}
+	return (numz);
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - checks main
+ * @n: an input number
+ * Return: 0 or 1
+*/
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1)
+}
