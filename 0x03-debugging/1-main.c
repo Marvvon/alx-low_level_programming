bb631a69aa0629063a6abea61ed6a4b85279c1a0
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-07 09:33:55 -0700

    pls work for

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,3 @@
- Bytes
 #include <stdio.h>
 /**
  * main - causes an infinite loop
@@ -12,10 +11,10 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)*/
-	/* {*/
-	/*	putchar(i);*/
-	/* }*/
+	/* while (i < 10) */
+	/* { */
+	/*	putchar(i); */
+	/* } */
 
 	printf("Infinite loop avoided! \\o/\n");
 
