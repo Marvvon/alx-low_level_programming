b0607d4853d6746f0c11eb05628dedbc9b533d5d
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-12 05:19:42 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps valuse of two integers
+ * @a,b,p,pp: integers
+ * Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	a = *p;
+	b = *pp;
+	*p = *pp;
+}
