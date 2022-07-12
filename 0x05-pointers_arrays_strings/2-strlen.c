a653d947ce2c472fe59745674546a3849f7d9e78
Marvellous Osanebi <oghenebruche04@gmail.com>
2022-07-12 06:31:43 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strlen - returns the length of a string
+ *
+ * @s: function parameter
+ *
+ * Return: 0
+*/
+int _strlen(char *s)
+{
+	int n = strlen(s);
+
+	return (n);
+
+}
